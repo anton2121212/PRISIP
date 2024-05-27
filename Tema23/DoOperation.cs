be77106822aa283e2f899e2895ab59a826f27e0b
anton2121212 <122678473+anton2121212@users.noreply.github.com>
2024-05-27 09:47:08 +0300

    Add files via upload

---
+++ Tema23/DoOperation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema23
+{
+    class DoOperation
+    {
+        public enum Operation
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        }
+        public static double Dooperation(double x, double y, Operation op)
+        {
+            //double result = op switch
+            //{
+            //    Operation.Add => x + y,
+            //    Operation.Subtract => x - y,
+            //    Operation.Multiply => x * y,
+            //    Operation.Divide => x / y
+            //};
+            double result = 0;
+            switch (op )
+            {
+                case Operation.Add: result = x + y; break;
+                case Operation.Subtract: result = x - y; break;
+                case Operation.Multiply: result = x * y; break;
+                case Operation.Divide: result = x * y; break;
+                default: Console.WriteLine("ошибка");
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tema23/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema23
+{
+    class Program
+    {
+        //задача 1
+        //enum Month
+        //{
+        //    январь, февраль, март, апрель, май, июнь, июль, август, сентябрь, октябрь, ноябрь, декабрь
+        //}
+
+
+        //задача 2
+        //enum Days
+        //{
+        //    Sat, Sun, Mon, Tue, Wed, Thu, Fri 
+        //}
+
+        //задача 3
+       
+  
+        static void Main(string[] args)
+        {
+            //задача 1
+            //Month i;
+            //for (i=Month.январь; i<=Month.декабрь; i++)
+            //{
+            //    Console.WriteLine($"{(int)i}      {i}");
+            //}
+
+
+            //задача 2
+            //Days i;
+            //for (i = Days.Sat; i <= Days.Fri; i++)
+            //{
+            //    Console.WriteLine($"{i} имеет значение {(int)i}");
+            //}
+
+
+            //задача 3
+
+            Console.WriteLine("Введите первое число=");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число=");
+            double y = double.Parse(Console.ReadLine());
+
+
+
+
+            DoOperation.Dooperation(x, y, DoOperation.Operation.Add);
+            DoOperation.Dooperation(x, y, DoOperation.Operation.Subtract);
+            DoOperation.Dooperation(x, y, DoOperation.Operation.Multiply);
+            DoOperation.Dooperation(x, y, DoOperation.Operation.Divide);       
+
+            Console.ReadKey();
+        }
+    }
+}
